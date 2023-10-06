8e3ee208deec05eca004a1d519de68dbcec774d4
Neema_Mwende <gracepete12@gmail.com>
2023-10-06 18:39:05 +0300

    events

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,37 @@ const Home = () => {
         console.log('hello, ninjas');
     }
 
+    const handleClickk = (e) => 
+    {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name) =>
+    {
+        console.log('hello' + name);
+    }
+
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}> Click me</button>
+            <button onClick={handleClickk}> Click me</button>
+            {/* <button onClick={handleClickAgain}> Click me again</button> */}
+            <button onClick={ () => handleClickAgain(' mario ')}> Click me again</button>
         </div>
      );
 }
  
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
