ff203b3aeea37aeee204c6ebf12356d10c47ef23
Neema_Mwende <gracepete12@gmail.com>
2023-10-09 16:17:25 +0300

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    let name = 'Angel';
 
     const handleClick = () => 
     {
-        console.log('hello, ninjas');
+        name = 'Candace';
+        console.log( name );
     }
 
-    const handleClickk = (e) => 
+    /* const handleClickk = (e) => 
     {
         console.log('hello, ninjas', e);
     }
@@ -13,16 +18,18 @@ const Home = () => {
     const handleClickAgain = (name) =>
     {
         console.log('hello' + name);
-    }
+    } */
 
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
             <button onClick={handleClick}> Click me</button>
-            <button onClick={handleClickk}> Click me</button>
+            {/* <button onClick={handleClickk}> Click me</button> */}
             {/* <button onClick={handleClickAgain}> Click me again</button> */}
-            <button onClick={ () => handleClickAgain(' mario ')}> Click me again</button>
+             {/* anonymous function gets access to the function event automatically */}
+            {/* <button onClick={ () => handleClickAgain(' mario ')}> Click me again</button> */}
         </div>
      );
 }
