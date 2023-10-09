0b77516600b68d9ba73b802d30709908157cb782
Neema_Mwende <gracepete12@gmail.com>
2023-10-09 17:34:17 +0300

    new home component

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,21 @@
 import { useState } from "react";
 
-const Home = () => {
-
-    /* let name = 'Angel'; */
-    /* const [name, setName] = useState('angel');
-    const [age, setAge] = useState(25);
-    const handleClick = () => 
-    {
-        /* name = 'Candace';
-        console.log( name ); */
-        /* setName('mario');
-        setAge(30); */
-
-    }
-
-    /* const handleClickk = (e) => 
-   /*  {
-        console.log('hello, ninjas', e);
-    }
-
-    const handleClickAgain = (name) =>
-    {
-        console.log('hello' + name);
-    } */
-
-
+const Home = () => 
+{
+    const [blogs, useBlogs] = useState([
+        {title: 'My new Website', body: 'lorem lipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party', body: 'lorem lipsum...', author: 'angel', id: 2},
+        {title: 'Web dev top tips', body: 'lorem lipsum...', author: 'candice', id: 3}
+    ]);
+    
+     
+    
+    
     return ( 
         <div className="home">
-            {/* <h2>Homepage</h2>
-            <p>{ name } is { age } years old </p>
-            <button onClick={handleClick}> Click me</button> */}
-            {/* <button onClick={handleClickk}> Click me</button> */}
-            {/* <button onClick={handleClickAgain}> Click me again</button> */}
-             {/* anonymous function gets access to the function event automatically */}
-            {/* <button onClick={ () => handleClickAgain(' mario ')}> Click me again</button> */}
+
         </div>
      );
 }
-
  
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
