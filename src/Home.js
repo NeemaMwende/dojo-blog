6a16925c2ec21e93cd9f87cf942b23802cae7c10
Neema_Mwende <gracepete12@gmail.com>
2023-10-16 16:12:43 +0300

    outputting lists

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,12 @@ const Home = () =>
     
     return ( 
         <div className="home">
-
+             {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}> 
+                    <h2> { blog.title } </h2>
+                    <p> written by { blog.author }</p>
+                </div>
+             ))}
         </div>
      );
 }
