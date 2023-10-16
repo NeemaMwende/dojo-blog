dcd71bf66d4d9dffbe86a8182c4f4bcebdac1dae
Neema_Mwende <gracepete12@gmail.com>
2023-10-16 22:22:58 +0300

    propss

---
+++ src/BlogList.js
@@ -0,0 +1,21 @@
+
+
+const BlogList = (props) => {
+/*     receiving the props from the home component */
+    const blogs = props.blogs;
+    console.log(props,blogs);
+
+
+    return (   
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}> 
+                    <h2> { blog.title } </h2>
+                    <p> Written by { blog.author }</p>
+                </div>
+             ))}
+        </div>  
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
-import BlogList from "./blog-lists";
+import BlogList from "./BlogList";
 
 const Home = () => 
 {
-    
-     
+    const [blogs, setBlogs] = useState([
+        {title: 'My new Website', body: 'lorem lipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party', body: 'lorem lipsum...', author: 'angel', id: 2},
+        {title: 'Web dev top tips', body: 'lorem lipsum...', author: 'candice', id: 3}
+    ]);
+      
     
     return ( 
         <div className="home">
-             <BlogList />
+            {/* props enable one to pass data from the parent component
+            to the child component: home to bloglist */}
+             <BlogList blogs={blogs} />
         </div>
      );
 }
