c4c8cba7e5ed08acd81313f2a31cb4bedb48594c
Neema_Mwende <gracepete12@gmail.com>
2023-10-17 17:22:17 +0300

    props as functions

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
+    const handleDelete = props.handleDelete;
     /* console.log(props, blogs); */
 
     return ( 
@@ -10,6 +11,7 @@ const BlogList = (props) => {
             <div className="blog-preview">
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
+                <button onClick={ () => handleDelete(blog.id)}> Delete Blog </button>
             </div>
         ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { id: 1, title: 'First Blog', content: 'This is the first blog.', author: 'Angel' },
         { id: 1, title: 'Second Blog', content: 'This is the first blog.', author: 'Neema' },
-        { id: 1, title: 'Third Blog', content: 'This is the first blog.', author: 'Grace' }
+        { id: 1, title: 'Third Blog', content: 'This is the first blog.', author: 'Angel' }
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Angel')} title="Angel\s Blog" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Angel')} title="Angel's Blog" />
         </div>
      );
 }
