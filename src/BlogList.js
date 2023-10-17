bbd3b2ded0280f1a44e4b6e013f4eeea82718782
Neema_Mwende <gracepete12@gmail.com>
2023-10-17 18:18:08 +0300

    use effect

--- src/BlogList.js
+++ src/BlogList.js
@@ -10,7 +10,7 @@ const BlogList = ({ title, blogs, handleDelete}) =>  {
         <div className="blog-list">
             <h2> { title }</h2>
             {blogs.map((blog) => (
-            <div className="blog-preview">
+            <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
                 <button onClick={() => handleDelete(blog.id)}> Delete Blog </button>
