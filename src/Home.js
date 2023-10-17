c647fd56f0f85b95dabe929c94ad3b8912caadfb
Neema_Mwende <gracepete12@gmail.com>
2023-10-17 23:49:23 +0300

    fetching data

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,11 @@ import BlogList from "./BlogList";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+    const [blogs, setBlogs] = useState(/* [
         { id: 1, title: 'First Blog', content: 'This is the first blog.', author: 'Angel' },
         { id: 1, title: 'Second Blog', content: 'This is the first blog.', author: 'Neema' },
         { id: 1, title: 'Third Blog', content: 'This is the first blog.', author: 'Angel' }
-    ]);
+    ] */ null);
 
     const handleDelete = (id) => 
     {
@@ -20,9 +20,17 @@ const Home = () => {
     /* renders the component each time it loads */
    useEffect(() => /* good for fetching data */
    {
-    console.log('use effect ran');
-    console.log(name);
-   }, [name]);
+    /* this is a get request to localhost port 8000 */
+    fetch('http://localhost:8000/blogs')
+        .then(Response => 
+            {
+                return Response.json();
+            })
+        .then(data => 
+            {
+                console.log("Data received", data);
+            }) 
+   }, []);
 /*    Here, name is the dependancy, useEffect watches for it and 
    only renders once the button is clicked not for the other functions 
    , but still renders initialy on the 1st load*/
