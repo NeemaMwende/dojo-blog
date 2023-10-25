54f960b0b7643395a50170f3672ac160bf24a194
Neema_Mwende <gracepete12@gmail.com>
2023-10-25 15:53:39 +0300

    route on home component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import MyComponent from './MyComponent';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
-
-
-// This is the root component of the application which gets rendered to the DOM
-function App() {
-  // const title = "Welcome to the new blog";
-  // const likes = 50;
-  return (
-    <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-          <Home /> 
-        {/*  {/* <h1>{ title }</h1>
-          <p>Liked { likes }</p>
-          <p>{ Math.random() * 10 }</p>
-          <p>{ "Hello, World"}</p>
-          <p>{ [1, 2, 3, 4, 5] }</p> */}
-          {/* <a href={'link'}>Google Site</a> */}
-      </div>
-    </div>
-  </Router>
-  );
+function App()
+{
+    return 
+    (
+        <Router>
+            <div className="App">
+                <Navbar />
+                    <div className="content">
+                        <Switch>
+                            <Route path="/"> 
+                                <Home />
+                            </Route>    
+                        <Switch/>
+                    </div>
+            </div>
+        <Router/>
+    );
 }
 
-export default App;
- 
+export default 'App';
