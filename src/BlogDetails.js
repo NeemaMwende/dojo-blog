5d8990cd9e75f04a21cb19ce966d997ee48d9875
Neema_Mwende <gracepete12@gmail.com>
2023-10-27 13:28:21 +0300

    how to delete a blog and redirect to the home page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from './UseFetch';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
   
 const BlogDetails = () => 
 {
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+
+    const handleClick = () => 
+    {
+        fetch('http://localhost:8000/blogs/' + blog.id, 
+        {
+            method: 'DELETE'
+        }).then (() => 
+        {
+            history.push('/');
+        })
+    }
     return ( 
         <div className="blog-details">
             {/* <h2>Blog Details - { id }</h2> */}
@@ -17,6 +29,7 @@ const BlogDetails = () =>
                     <h2>{ blog.title }</h2>
                     <p>Written by  { blog.author }</p>
                     <div> { blog.content }</div>
+                    <button onClick={ handleClick }>Delete Blog</button>
                  </article>
             )}
         </div>
