8cb4b47d30f8ef494910ac225b69eddac1ca4f89
Neema_Mwende <gracepete12@gmail.com>
2023-10-27 10:35:23 +0300

    creating forms in the create component

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,25 @@
 const Create = () => {
     return ( 
         <div className="create">
-            <h2>This is a second route</h2>
+            <h2>Add a new Blog </h2>
+            <form>
+                <label> Blog Title: </label>
+                    <input 
+                    type='text' 
+                    placeholder='Enter title here...'
+                    required />
+                
+                <label> Blog body: </label>
+                    <textarea >
+                        required
+                    </textarea>
+                
+                <label> Blog author: </label>
+                <select >
+                    <option value="Angel"> Angel </option>
+                    <option value="Grace"> Grace </option>
+                </select>
+            </form>
         </div>
      );
 }
